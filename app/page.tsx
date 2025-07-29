c52a243e5825919e8822d81087a6ce55555558ee
amorcer-dev <amorcerdev@gmail.com>
2025-07-29 09:20:57 -0400

    Update website

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ClinicHero } from '../components/HeroSection'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ClinicHero
+        title="HealthCare Clinic"
+        subtitle="Professional healthcare services for you and your family"
+        ctaText="Book Appointment"
+        ctaLink="#contact"
+        className=""
+      />
+    </main>
+  );
+}
