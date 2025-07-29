7170e9fdff53460f8fdd71f2f11d51f6b37e1df1
amorcer-dev <amorcerdev@gmail.com>
2025-07-29 09:48:50 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App directory is now stable in Next.js 14
+  async headers() {
+    return [
+      {
+        // Adjust this to match ALL routes, or just the home page
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' }, // or remove this header
+          { key: 'Content-Security-Policy', value: 'frame-ancestors https://amorcer.com' }, // or add a wildcard, if needed, for local dev
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
